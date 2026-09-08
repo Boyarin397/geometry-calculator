c54991309bcb3ce4c946950f402ccd0b20250133
Boyarin397 <10superpuper01@gmail.com>
2026-09-08 15:31:43 +0400

    Add config message

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from circle import Circle
 from recangle import Rectangle
-print("Конфигурация загружена")
+  print("Конфигурация загружена")
+
 def main():
   print("Калькулятор геометрии запущен")
   print("Версия 1.0")
