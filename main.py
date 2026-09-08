d71c0884cc2bc4f1dc7f2dc31c1684112d9aab91
Boyarin397 <10superpuper01@gmail.com>
2026-09-08 15:26:32 +0400

    Update main.py with imports

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from circle import Circle
+from recangle import Rectangle
+
 def main():
-  print("Калькулятор геометрии")
-  print("В разработке...")
+  print("Калькулятор геометрии запущен")
+  print("Версия 1.0")
 
 if_name_=="_main_":
 main()
